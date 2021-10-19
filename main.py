e753cafaed021353b75b604849e7119b20978838
Pranoy Bhansali <pranoybhansali1@gmail.com>
2021-10-19 20:44:45 +0530

    upload file

--- main.py
+++ main.py
@@ -1,12 +1,37 @@
-from flask import Flask
-from flask import render_template
+import os
+from app import app
+import urllib.request
+from flask import Flask, flash, request, redirect, url_for, render_template
+from werkzeug.utils import secure_filename
 
-app = Flask(__name__)
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 
-@app.route("/")
-@app.route("/home")
-def home():
-	return render_template("try.html")
+def allowed_file(filename):
+	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+	
+@app.route('/')
+def upload_form():
+	return render_template('upload.html')
+
+@app.route('/', methods=['POST'])
+def upload_image():
+	if 'file' not in request.files:
+		flash('No file part')
+		return redirect(request.url)
+	file = request.files['file']
+	if file.filename == '':
+		flash('No image selected for uploading')
+		return redirect(request.url)
+	if file and allowed_file(file.filename):
+		filename = secure_filename(file.filename)
+		file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+		return render_template('upload.html', filename=filename)
+	else:
+		return redirect(request.url)
+
+@app.route('/display/<filename>')
+def display_image(filename):
+	return redirect(url_for('static', filename='uploads/' + filename), code=301)
 
 if __name__ == "__main__":
-	app.run(debug=True)
+    app.run()
