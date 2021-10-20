ba9045bfb758f666a5189d2bf5d8667bfad8f64d
Pranoy Bhansali <pranoybhansali1@gmail.com>
2021-10-20 18:47:24 +0530

    ready for integration

---
+++ static/main.js
@@ -0,0 +1,10 @@
+// resize header to size of browser window
+
+var ready = (callback) => {
+	if (document.readyState != "loading") callback();
+	else document.addEventListener("DOMContentLoaded", callback);
+}
+
+ready(() => { 
+	document.querySelector(".header").style.height = window.innerHeight + "px";
+})
